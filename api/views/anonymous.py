08aeaf1d106280515b6f93600d2b79589baa0068
Yoni <svechinskyy@gmail.com>
2020-04-24 20:23:36 +0300

    Bug fixes

--- api/views/anonymous.py
+++ api/views/anonymous.py
@@ -4,10 +4,9 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import AllowAny
 
-from api.serializers import (
-    AnonymousMetricsSerializer
-)
+from api.serializers import AnonymousMetricsSerializer
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -22,11 +21,13 @@ class AnonymousViewSet(ViewSet):
         request_body=AnonymousMetricsSerializer(),
         responses={200: AnonymousMetricsSerializer()},
     )
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, permission_classes=[AllowAny])
     def submit_anonymous_metrics(self, request):
         """Called to submit the full anonymous metrics, expects to receive all answers for metric questions and the
         recordings."""
-        ser = AnonymousMetricsSerializer(data=request.data, context={"request": request})
+        ser = AnonymousMetricsSerializer(
+            data=request.data, context={"request": request}
+        )
         ser.is_valid(raise_exception=True)
         ser.save()
         return Response(ser.data)
